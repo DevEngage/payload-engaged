0865a954d43111a933021cac58dbd0722f090c49
James Brinkerhoff <th3brink@gmail.com>
2025-01-30 15:56:59 -0700

    fixed builds

--- apps/website/src/endpoints/seed/contact-page.ts
+++ apps/website/src/endpoints/seed/contact-page.ts
@@ -7,41 +7,41 @@ export const contact: Partial<Page> = {
     type: 'none',
   },
   layout: [
-    {
-      blockType: 'formBlock',
-      enableIntro: true,
-      form: '{{CONTACT_FORM_ID}}',
-      introContent: {
-        root: {
-          type: 'root',
-          children: [
-            {
-              type: 'heading',
-              children: [
-                {
-                  type: 'text',
-                  detail: 0,
-                  format: 0,
-                  mode: 'normal',
-                  style: '',
-                  text: 'Example contact form:',
-                  version: 1,
-                },
-              ],
-              direction: 'ltr',
-              format: '',
-              indent: 0,
-              tag: 'h3',
-              version: 1,
-            },
-          ],
-          direction: 'ltr',
-          format: '',
-          indent: 0,
-          version: 1,
-        },
-      },
-    },
+    // {
+    //   blockType: 'formBlock',
+    //   enableIntro: true,
+    //   form: '{{CONTACT_FORM_ID}}',
+    //   introContent: {
+    //     root: {
+    //       type: 'root',
+    //       children: [
+    //         {
+    //           type: 'heading',
+    //           children: [
+    //             {
+    //               type: 'text',
+    //               detail: 0,
+    //               format: 0,
+    //               mode: 'normal',
+    //               style: '',
+    //               text: 'Example contact form:',
+    //               version: 1,
+    //             },
+    //           ],
+    //           direction: 'ltr',
+    //           format: '',
+    //           indent: 0,
+    //           tag: 'h3',
+    //           version: 1,
+    //         },
+    //       ],
+    //       direction: 'ltr',
+    //       format: '',
+    //       indent: 0,
+    //       version: 1,
+    //     },
+    //   },
+    // },
   ],
   title: 'Contact',
 }
